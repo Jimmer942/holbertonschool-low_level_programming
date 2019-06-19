942fbd246f53c4f370f615b6fdd0cd25f36fd1d1
Jimmer942 <942@holbertonschool.com>
2019-06-19 10:48:38 -0500

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/*Prints on the terminal Programming is like building a multilingual puzzle */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
