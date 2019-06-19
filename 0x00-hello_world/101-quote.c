97fb615adc903a32f686d7aa5d55d328aad86afe
Jimmer942 <942@holbertonschool.com>
2019-06-19 13:49:12 -0500

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity #advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * main - and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
  * Return: 1 if compilation is succesfully, we write on the standart error
  */
 int main(void)
 {
-	fprintf( stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char  *frase = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int a = strlen(frase);
+	write(2, frase, a);
 	return (1);
 }
