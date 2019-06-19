f38228d8b3c3d15e2d8f058f3ebad0bd1d5727eb
Jimmer942 <942@holbertonschool.com>
2019-06-19 12:43:33 -0500

    Write a C program that prints exactly  followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+ * Return: 1 if compilation is succesfully, we write on the standart error
+ */
+int main(void)
+{
+	fprintf( stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
