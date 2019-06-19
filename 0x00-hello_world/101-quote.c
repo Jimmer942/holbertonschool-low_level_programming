bb7bee0c1a8244c7b8af2305a0118dbb9cd734b8
Jimmer942 <942@holbertonschool.com>
2019-06-19 14:12:37 -0500

    101-quote.c FIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <string.h>
 /**
  * main - and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
  * Return: 1 if compilation is succesfully, we write on the standart error
@@ -7,8 +6,7 @@
 int main(void)
 {
 	char *pha = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	int len = strlen(pha) + 1;
 
-	write(2, pha, len);
+	write(2, pha, 59);
 	return (1);
 }
