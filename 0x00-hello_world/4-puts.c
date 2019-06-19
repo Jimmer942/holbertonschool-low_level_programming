305ce9e2daa027106f40b64099716b7f32ef7322
Jimmer942 <942@holbertonschool.com>
2019-06-19 12:13:39 -0500

    README

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*Prints on the terminal Programming is like building a multilingual puzzle */
+/**
+ * main - Prints Programming is like building a multilingual puzzle
+ * Return: 0 is compile succesfully
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*Prints "with proper grammar, but the outcome is a piece of art"*/
+/**
+ * main - Prints with proper grammar, but the outcome is a piece of art
+ * Return: 0 if compilation is succesfully
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art\n");
