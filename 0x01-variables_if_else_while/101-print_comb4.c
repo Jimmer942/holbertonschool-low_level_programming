9c9736dd2e91b5b1a3e5416c8d02ca1c4ba4ce3d
Jimmer942 <942@holbertonschool.com>
2019-06-20 23:17:56 -0500

    Second advance task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print combination of three-digit numbers
+ * Return: 0 if compilation is succesfully
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			for (k = j + 1; k < 58; k++)
+			{
+				if (j != k || j != i || k != i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56 || k != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
