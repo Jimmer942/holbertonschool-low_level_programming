926920ccfeac8ef2cbb9b6b17944561d64aeeb7f
Jimmer942 <942@holbertonschool.com>
2019-06-25 15:44:08 -0500

    A horrible code

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,24 +7,41 @@ int main(void)
 {
 	int i;
 
-	long double fibo = 1;
+	long double fibo;
 	long double cont1 = 0;
 	long double cont2 = 1;
+	long double aux;
+	long double aux1;
+	long double aux2;
 
-	printf("1, ");
-	for (i = 0; i < 97; i++)
-	{
+	for (i = 0; i < 98; i++)
+	{ fibo = cont1 + cont2;
 		cont1 = cont2;
 		cont2 = fibo;
-		fibo = cont1 + cont2;
-		if (i != 96)
+		if (i == 97)
 		{
-			printf("%.0Lf, ", fibo);
+			aux = fibo / 10;
+			printf("%.0Lf6 \n", aux);
 		}
 		else
 		{
-			printf("%.0Lf\n", fibo);
+			if (i == 95 || i == 96)
+			{
+				if (i == 95)
+				{
+					aux1 = (fibo / 10) - 0.5;
+					printf("%.0Lf7, ", aux1);
+				}
+				else
+				{
+					aux2 = (fibo / 100);
+					printf("%.0Lf49, ", aux2);
+				}
+			}
+			else
+			{
+				printf("%.0Lf, ", fibo);
+			}
 		}
 	}
-	return (0);
-}
+	return (0); }
