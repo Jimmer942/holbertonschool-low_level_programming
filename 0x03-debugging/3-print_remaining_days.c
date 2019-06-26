c9a09f208d7edf31309f89272c319eadb2cca48e
Jimmer942 <942@holbertonschool.com>
2019-06-25 22:26:13 -0500

    Finlly going to sleep

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,19 +2,19 @@
 #include "holberton.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
- */
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
+*/
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
 	{
-		if (month <= 12 && day <= 30)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
@@ -24,9 +24,9 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month > 12 && day > 30)
+		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
