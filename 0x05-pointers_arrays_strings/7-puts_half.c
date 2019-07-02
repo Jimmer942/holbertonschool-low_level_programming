578dfd91aa113b921b830c637a958640dd1a9a54
Jimmer942 <942@holbertonschool.com>
2019-07-02 09:49:14 -0500

    7-puts_half.c Fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,11 +13,7 @@ void puts_half(char *str)
 	{
 		count++;
 	}
-	if (count % 2 != 0)
-	{
-		count--;
-	}
-	for (i = count / 2; i <= count; i++)
+	for (i = count / 2; i < count; i++)
 	{
 		_putchar(str[i]);
 	}
