b22cbcadc3a49562a476c2cc9b6eceb8c9ef2ec8
Jimmer942 <942@holbertonschool.com>
2019-07-02 10:39:46 -0500

    7-puts_half.c fix

---
+++ 7-puts_half.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * puts_half - prints one char out of 2 of a string, followed by a new line.
+ * @str: The string to print
+ * Return: 0 if compilation is succesfully
+ */
+void puts_half(char *str)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		count++;
+	}
+	if (count == 1)
+	{
+		_putchar(str[0]);
+		_putchar('\n');
+	}
+	else
+	{
+		if (count % 2 != 0)
+		{
+			count++;
+		}
+		else
+		{
+			for (i = count / 2; i < count; i++)
+			{
+				_putchar(str[i]);
+			}
+		}
+		_putchar('\n');
+	}
+}
