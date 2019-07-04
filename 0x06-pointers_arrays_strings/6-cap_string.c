770f3c0c465b7d06870bc5d7eeb8724a8f7b0654
Jimmer942 <942@holbertonschool.com>
2019-07-04 13:04:45 -0500

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * cap_string - function that capitalizes all words of a string.
+ * @s: String to uppercase..
+ * Return: s on uppercase.
+ */
+char *cap_string(char *s)
+{
+	char pr[13] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
+	int i, j;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+	{
+		for (j = 0 ; pr[j] != '\0'; j++)
+		{
+			if (s[i] == pr[j])
+			{
+				if (s[i + 1] > 96 && s[i + 1] < 123)
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
