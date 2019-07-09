859ebd5d1ab5701dfc7b108b81a6c00a1decaa81
Jimmer942 <942@holbertonschool.com>
2019-07-08 21:22:37 -0500

    8-print_diagsums.c fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,18 +8,21 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, d1, d2, diag1, diag2;
+	int i, j, d1, d2, diag1 = 0, diag2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
-                d1 = j * (size + 1);
-	        d2 = (j + 1) * (size - 1);
-		diag1 = diag1 + *(a + d1);
-		diag2 = diag2 + *(a + d2);
+			if (j == i)
+			{
+				d1 = j * (size + 1);
+				d2 = (j + 1) * (size - 1);
+				diag1 = diag1 + *(a + d1);
+				diag2 = diag2 + *(a + d2);
+			}
 		}
 	}
-	printf("%d ", diag1);
+	printf("%d, ", diag1);
 	printf("%d\n", diag2);
 }
