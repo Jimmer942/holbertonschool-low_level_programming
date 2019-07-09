3bb0eda57a3d0175e75d80654f6af839a21868b2
Jimmer942 <51680350+Jimmer942@users.noreply.github.com>
2019-07-08 19:43:34 -0500

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,13 +14,10 @@ void print_diagsums(int *a, int size)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (j == i)
-			{
-				d1 = j * (size + 1);
-				d2 = (j + 1) * (size - 1);
-				diag1 = diag1 + *(a + d1);
-				diag2 = diag2 + *(a + d2);
-			}
+                d1 = j * (size + 1);
+	        d2 = (j + 1) * (size - 1);
+		diag1 = diag1 + *(a + d1);
+		diag2 = diag2 + *(a + d2);
 		}
 	}
 	printf("%d ", diag1);
