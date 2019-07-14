c5965107e73670975ca90fa3dd396588dc733193
Jimmer942 <942@holbertonschool.com>
2019-07-14 12:49:38 -0500

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - is a program that prints all arguments it receives.
+ * @argc: argument count or size of arguments.
+ * @argv: argument vector
+ * Return: always 0 if all arguments are numbers otherwise 1.
+ */
+int main(int argc, char *argv[])
+{
+	int add = 0, i;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0 && i != 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			add = add + atoi(argv[i]);
+		}
+	}
+	printf("%i\n", add);
+	return (0);
+}
