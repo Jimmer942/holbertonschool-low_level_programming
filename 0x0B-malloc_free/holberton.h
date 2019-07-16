33fc8ddd97e0aefe08815065990fd6a183611d10
Jimmer942 <942@holbertonschool.com>
2019-07-16 12:03:27 -0500

    Midday work

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,10 @@
+#ifndef INV_HOLBERTON_H
+#define INV_HOLBERTON_H
+void free_grid(int **grid, int height);
+char **strtow(char *str);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+#endif
