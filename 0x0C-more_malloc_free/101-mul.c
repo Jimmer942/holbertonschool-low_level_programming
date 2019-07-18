1edd9b4e8e73280ace05adec31f700ddf4827ac2
Jimmer942 <942@holbertonschool.com>
2019-07-18 14:21:13 -0500

    Looking for digits if not exit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - a program that multiplies two numbers.
  * @argc: argument count or size of arguments.
@@ -8,24 +9,24 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul, i;
+	int mul, i, j;
 
 	if (argc == 3)
 	{
-		for (i = 0; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) == 0 && i != 0)
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				mul = atoi(*(argv + 1)) * atoi(*(argv + 2));
-				printf("%i\n", mul);
-				return (0);
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					exit(98);
+				}
 			}
 		}
+		mul = atoi(*(argv + 1)) * atoi(*(argv + 2));
+		printf("%i\n", mul);
+		return (0);
 	}
 	else
 	{
