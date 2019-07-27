fea24200c241f9cdd706e3c4641ece6cf261efeb
Jimmer942 <942@holbertonschool.com>
2019-07-27 12:41:47 -0500

    Advance task

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .text
+	global main
+
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text
+	mov rdx, 14
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+section .data
+        text db "Hello, World!",10
