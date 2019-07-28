ce508e501b7bad6f602950c6479f293d592ec66d
Jimmer942 <942@holbertonschool.com>
2019-07-27 20:27:37 -0500

    Final task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,16 @@
 #ifndef HOLBERTON
 #define HOLBERTON
+/**
+ * struct format - Struct format.
+ * @id: The identificator.
+ * @f: The function associated.
+ */
+typedef struct format
+{
+	char *id;
+	void (*f)();
+} ident;
+void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
