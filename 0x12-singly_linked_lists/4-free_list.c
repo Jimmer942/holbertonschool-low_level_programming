81b318d1232e80e9691085bf4ee7d3ad61dd9554
Jimmer942 <942@holbertonschool.com>
2019-08-01 17:04:21 -0500

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * free_list - is a function that frees a list_t list.
+ * @head: linked list to free.
+ */
+void free_list(list_t *head)
+{
+	while(head != NULL)
+	{
+		free(head->str);
+		free(head);
+	}
+}
