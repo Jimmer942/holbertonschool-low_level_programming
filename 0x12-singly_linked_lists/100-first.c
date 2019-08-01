3f1cfe0cfb468f36feb03218dc60e83c19c9d137
Jimmer942 <942@holbertonschool.com>
2019-08-01 17:26:58 -0500

    Last advance task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * my_constructor - is a function that prints:
+ * "You're beat! and yet, you must allow,\n
+ * I bore my house upon my back!\n"
+ * before the main function is executed.
+ * @h: linked list to print.
+ * Return: the length of the list.
+ */
+
+void my_constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
