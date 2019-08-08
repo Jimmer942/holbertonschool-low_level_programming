df7723eb42eaceb825eb0d5592214d0b0ca48a24
Jimmer942 <942@holbertonschool.com>
2019-08-08 13:40:50 -0500

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_binary - Function that prints the binary representation of a number.
+ * @n: Number to convert.
+ */
+void print_binary(unsigned long int n)
+{
+	int i, a = 1, b, flag = 0;
+	unsigned long int p;
+
+	if (n == 0)
+	{
+		_putchar(48);
+		return;
+	}
+	for (i = 0; i < 64; i++)
+	{
+		p = ((a << (63 - i)) & n);
+		if (p >> (63 - i))
+			flag = 1;
+		if (flag)
+		{
+			b = p >> (63 - i);
+			_putchar(b + 48);
+		}
+	}
+}
