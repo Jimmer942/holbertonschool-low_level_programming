52783a3bf2c74733629c0ead66a345da797c7056
Jimmer942 <942@holbertonschool.com>
2019-08-13 19:51:15 -0500

    Permissions

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY, 0);
+	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
 		return (0);
 	lt = malloc(letters);
