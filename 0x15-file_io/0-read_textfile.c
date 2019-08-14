cdf97f85bf555bdb19aa534ea5c2494a0f53287c
Jimmer942 <942@holbertonschool.com>
2019-08-13 19:48:21 -0500

    Malloc

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
  * and prints it to the POSIX standard output.
  * @filename: File to print.
  * @letters:  Amount of letters to print.
- * Return: The number of characters that can be printed.
+ * Return: The number of characters that can be written.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY, 0);
 	if (fd == -1)
 		return (0);
-	lt = calloc(letters, sizeof(char));
+	lt = malloc(letters);
 	if (lt == NULL)
 		return (0);
 	n = read(fd, lt, letters);
