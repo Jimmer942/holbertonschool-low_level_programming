7c7e3b88f038450a0692c8cc94b630436d90691e
Jimmer942 <942@holbertonschool.com>
2019-09-14 19:34:50 -0400

    c_7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,8 +11,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 	dlistint_t *before, *after, *new;
 
-	if (h == NULL)
-		return (NULL);
 	if (*h == NULL && idx != 0)
 		return (NULL);
 	while ((**h).prev != NULL)
