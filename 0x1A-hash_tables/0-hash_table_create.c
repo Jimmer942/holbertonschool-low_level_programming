ff129a38d9f191e42b786011fe539a1333bd6e34
Jimmer942 <942@holbertonschool.com>
2019-10-11 19:19:40 -0400

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_table = malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t) * size);
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
