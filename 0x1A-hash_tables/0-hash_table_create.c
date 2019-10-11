15860b261e5b578119b8cb42cd9bafd8c5d32d3f
Jimmer942 <942@holbertonschool.com>
2019-10-11 19:25:05 -0400

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,16 +9,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 	hash_node_t **new_node;
+	unsigned long int i;
 
 	if (size == 0)
 		return (NULL);
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = malloc(sizeof(hash_node_t) * size);
 	if (new_node == NULL)
 		return (NULL);
-	new_table = malloc(sizeof(hash_table_t) * size);
+	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
 	new_table->array = new_node;
+	for (i = 0; i < size; i++)
+		new_node[i] = NULL;
 	return (new_table);
 }
