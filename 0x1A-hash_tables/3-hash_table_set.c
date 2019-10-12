a3ecdc5a45dbe021c2dd2d759db805254a535b32
Jimmer942 <942@holbertonschool.com>
2019-10-11 20:40:46 -0400

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,7 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || strlen(key) == 0)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
