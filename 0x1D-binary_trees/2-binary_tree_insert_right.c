50b86bf22532a2f8ceecd7375233a61892fc1cf5
Jimmer942 <942@holbertonschool.com>
2019-12-05 16:02:34 -0500

    2-binary_tree_insert_right.c

---
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right -
+ * @parent: parent node
+ * @value: int of the new node
+ * Return: pointer to the new node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *r;
+
+
+	if  (parent == NULL)
+		return (NULL);
+
+	r = malloc(sizeof(binary_tree_t));
+
+	if (r == NULL)
+		return (NULL);
+
+	r->n = value;
+	r->parent = parent;
+	r->left = NULL;
+	r->right = parent->right;
+	parent->right = r;
+
+	if (r->right)
+		r->right->parent = r;
+
+	return (r);
+}
